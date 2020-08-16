5736d6648fd4baa8c8f26e55529a54eb263327c0
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2020-08-17 04:14:11 +0900

    Load, parse, decode and display CSV data

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,11 @@
 module Main exposing (main)
 
 import Browser exposing (Document)
-import Html exposing (main_, text)
+import Csv
+import Csv.Decode as CD exposing (Decoder, Errors(..))
+import Html exposing (main_, table, tbody, td, text, th, thead, tr)
+import Http
+import Parser exposing (deadEndsToString)
 
 
 main : Program () Model Msg
@@ -19,13 +23,71 @@ main =
 
 
 type alias Model =
-    {}
+    { lapRecords : List LapRecord
+    }
+
+
+type alias LapRecord =
+    { carNumber : String
+    , driverNumber : String
+    , lapNumber : String
+    , lapTime : String
+    , lapImprovement : String
+    , crossingFinishLineInPit : String
+    , s1 : String
+    , s1Improvement : String
+    , s2 : String
+    , s2Improvement : String
+    , s3 : String
+    , s3Improvement : String
+    , kph : String
+    , elapsed : String
+    , hour : String
+    , topSpeed : String
+    , driverName : String
+    , pitTime : String
+    , class : String
+    , group : String
+    , team : String
+    , manufacturer : String
+    }
+
+
+lapRecordDecoder : Decoder (LapRecord -> a) a
+lapRecordDecoder =
+    CD.map LapRecord
+        (CD.field "NUMBER" Ok
+            |> CD.andMap (CD.field "DRIVER_NUMBER" Ok)
+            |> CD.andMap (CD.field "LAP_NUMBER" Ok)
+            |> CD.andMap (CD.field "LAP_TIME" Ok)
+            |> CD.andMap (CD.field "LAP_IMPROVEMENT" Ok)
+            |> CD.andMap (CD.field "CROSSING_FINISH_LINE_IN_PIT" Ok)
+            |> CD.andMap (CD.field "S1" Ok)
+            |> CD.andMap (CD.field "S1_IMPROVEMENT" Ok)
+            |> CD.andMap (CD.field "S2" Ok)
+            |> CD.andMap (CD.field "S2_IMPROVEMENT" Ok)
+            |> CD.andMap (CD.field "S3" Ok)
+            |> CD.andMap (CD.field "S3_IMPROVEMENT" Ok)
+            |> CD.andMap (CD.field "KPH" Ok)
+            |> CD.andMap (CD.field "ELAPSED" Ok)
+            |> CD.andMap (CD.field "HOUR" Ok)
+            |> CD.andMap (CD.field "TOP_SPEED" Ok)
+            |> CD.andMap (CD.field "DRIVER_NAME" Ok)
+            |> CD.andMap (CD.field "PIT_TIME" Ok)
+            |> CD.andMap (CD.field "CLASS" Ok)
+            |> CD.andMap (CD.field "GROUP" Ok)
+            |> CD.andMap (CD.field "TEAM" Ok)
+            |> CD.andMap (CD.field "MANUFACTURER" Ok)
+        )
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( {}
-    , Cmd.none
+    ( { lapRecords = [] }
+    , Http.get
+        { url = "23_Analysis_Race_Hour 6.csv"
+        , expect = Http.expectString Loaded
+        }
     )
 
 
@@ -34,14 +96,32 @@ init _ =
 
 
 type Msg
-    = NoOp
+    = Loaded (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
+        Loaded (Ok csvString) ->
+            ( case decodeString lapRecordDecoder csvString of
+                Ok lapRecords ->
+                    { model | lapRecords = lapRecords }
+
+                Err _ ->
+                    model
+            , Cmd.none
+            )
+
+        Loaded (Err _) ->
             ( model, Cmd.none )
+
+
+decodeString : Decoder (a -> a) a -> String -> Result Errors (List a)
+decodeString decoder =
+    Csv.parseWith ';'
+        >> Result.map (\csv -> { csv | headers = List.map String.trim csv.headers })
+        >> Result.mapError (deadEndsToString >> List.singleton >> CsvErrors)
+        >> Result.andThen (CD.decodeCsv decoder)
 
 
 
@@ -49,9 +129,25 @@ update msg model =
 
 
 view : Model -> Document Msg
-view _ =
+view { lapRecords } =
     { title = ""
     , body =
-        [ main_ [] [ text "elm-supercell" ]
+        [ main_ []
+            [ table []
+                [ thead []
+                    [ tr [] <|
+                        List.map (\heading -> th [] [ text heading ])
+                            [ "NUMBER", "LAP_NUMBER", "LAP_TIME", "S1", "S2", "S3", "KPH", "ELAPSED", "HOUR", "TOP_SPEED", "DRIVER_NAME", "PIT_TIME", "CLASS", "GROUP", "TEAM", "MANUFACTURER" ]
+                    ]
+                , tbody [] <|
+                    List.map
+                        (\lap ->
+                            tr [] <|
+                                List.map (\getter -> td [] [ text <| getter lap ])
+                                    [ .carNumber, .lapNumber, .lapTime, .s1, .s2, .s3, .kph, .elapsed, .hour, .topSpeed, .driverName, .pitTime, .class, .group, .team, .manufacturer ]
+                        )
+                        lapRecords
+                ]
+            ]
         ]
     }
